8ee3ec83210f2a53c2a103b46fab0a1e6aad3880
Duy Thai <duyhthai@gmail.com>
2024-04-03 14:41:03 +0700

    Azure AI Speech

--- Labfiles/07-speech/C-Sharp/speaking-clock/Program.cs
+++ Labfiles/07-speech/C-Sharp/speaking-clock/Program.cs
@@ -3,69 +3,97 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 
 // Import namespaces
-
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
 
 namespace speaking_clock
 {
-    class Program
+  class Program
+  {
+    private static SpeechConfig speechConfig;
+    static async Task Main(string[] args)
     {
-        private static SpeechConfig speechConfig;
-        static async Task Main(string[] args)
+      try
+      {
+        // Get config settings from AppSettings
+        IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+        IConfigurationRoot configuration = builder.Build();
+        string aiSvcKey = configuration["SpeechKey"];
+        string aiSvcRegion = configuration["SpeechRegion"];
+
+        // Configure speech service
+        speechConfig = SpeechConfig.FromSubscription(aiSvcKey, aiSvcRegion);
+        Console.WriteLine("Ready to use speech service in " + speechConfig.Region);
+
+        // Configure voice
+        speechConfig.SpeechSynthesisVoiceName = "en-US-AriaNeural";
+
+        // Get spoken input
+        string command = "";
+        command = await TranscribeCommand();
+        if (command.ToLower() == "what time is it?")
         {
-            try
-            {
-                // Get config settings from AppSettings
-                IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
-                IConfigurationRoot configuration = builder.Build();
-                string aiSvcKey = configuration["SpeechKey"];
-                string aiSvcRegion = configuration["SpeechRegion"];
-
-                // Configure speech service
-
-
-                // Get spoken input
-                string command = "";
-                command = await TranscribeCommand();
-                if (command.ToLower()=="what time is it?")
-                {
-                    await TellTime();
-                }
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+          await TellTime();
         }
 
-        static async Task<string> TranscribeCommand()
-        {
-            string command = "";
-            
-            // Configure speech recognition
-
-
-            // Process speech input
-
-
-            // Return the command
-            return command;
-        }
-
-        static async Task TellTime()
-        {
-            var now = DateTime.Now;
-            string responseText = "The time is " + now.Hour.ToString() + ":" + now.Minute.ToString("D2");
-                        
-            // Configure speech synthesis
-
-
-            // Synthesize spoken output
-
-
-            // Print the response
-            Console.WriteLine(responseText);
-        }
-
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
     }
+
+    static async Task<string> TranscribeCommand()
+    {
+      string command = "";
+
+      // Configure speech recognition
+      using AudioConfig audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+      using SpeechRecognizer speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+      Console.WriteLine("Speak now...");
+
+      // Process speech input
+      SpeechRecognitionResult speech = await speechRecognizer.RecognizeOnceAsync();
+      if (speech.Reason == ResultReason.RecognizedSpeech)
+      {
+        command = speech.Text;
+        Console.WriteLine(command);
+      }
+      else
+      {
+        Console.WriteLine(speech.Reason);
+        if (speech.Reason == ResultReason.Canceled)
+        {
+          var cancellation = CancellationDetails.FromResult(speech);
+          Console.WriteLine(cancellation.Reason);
+          Console.WriteLine(cancellation.ErrorDetails);
+        }
+      }
+
+      // Return the command
+      return command;
+    }
+
+    static async Task TellTime()
+    {
+      var now = DateTime.Now;
+      string responseText = "The time is " + now.Hour.ToString() + ":" + now.Minute.ToString("D2");
+
+      // Configure speech synthesis
+      // speechConfig.SpeechSynthesisVoiceName = "en-GB-RyanNeural";
+      speechConfig.SpeechSynthesisVoiceName = "en-GB-LibbyNeural";
+      using SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer(speechConfig);
+
+      // Synthesize spoken output
+      SpeechSynthesisResult speak = await speechSynthesizer.SpeakTextAsync(responseText);
+      if (speak.Reason != ResultReason.SynthesizingAudioCompleted)
+      {
+        Console.WriteLine(speak.Reason);
+      }
+
+      // Print the response
+      Console.WriteLine(responseText);
+    }
+
+  }
 }

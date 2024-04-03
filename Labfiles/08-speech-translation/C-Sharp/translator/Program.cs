18e65b08e833a5cb35891eeb7bfc87a8a58362a3
Duy Thai <duyhthai@gmail.com>
2024-04-03 15:02:53 +0700

    AI Speech - translator

--- Labfiles/08-speech-translation/C-Sharp/translator/Program.cs
+++ Labfiles/08-speech-translation/C-Sharp/translator/Program.cs
@@ -5,68 +5,93 @@ using System.Collections.Generic;
 using System.Text;
 
 // Import namespaces
-
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.CognitiveServices.Speech.Translation;
 
 namespace speech_translation
 {
-    class Program
+  class Program
+  {
+    private static SpeechConfig speechConfig;
+    private static SpeechTranslationConfig translationConfig;
+
+    static async Task Main(string[] args)
     {
-        private static SpeechConfig speechConfig;
-        private static SpeechTranslationConfig translationConfig;
+      try
+      {
+        // Get config settings from AppSettings
+        IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+        IConfigurationRoot configuration = builder.Build();
+        string aiSvcKey = configuration["SpeechKey"];
+        string aiSvcRegion = configuration["SpeechRegion"];
 
-        static async Task Main(string[] args)
+        // Set console encoding to unicode
+        Console.InputEncoding = Encoding.Unicode;
+        Console.OutputEncoding = Encoding.Unicode;
+
+        // Configure translation
+        translationConfig = SpeechTranslationConfig.FromSubscription(aiSvcKey, aiSvcRegion);
+        translationConfig.SpeechRecognitionLanguage = "en-US";
+        translationConfig.AddTargetLanguage("fr");
+        translationConfig.AddTargetLanguage("es");
+        translationConfig.AddTargetLanguage("hi");
+        Console.WriteLine("Ready to translate from " + translationConfig.SpeechRecognitionLanguage);
+
+        // Configure speech
+        speechConfig = SpeechConfig.FromSubscription(aiSvcKey, aiSvcRegion);
+
+        string targetLanguage = "";
+        while (targetLanguage != "quit")
         {
-            try
-            {
-                // Get config settings from AppSettings
-                IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
-                IConfigurationRoot configuration = builder.Build();
-                string aiSvcKey = configuration["SpeechKey"];
-                string aiSvcRegion = configuration["SpeechRegion"];
-
-                // Set console encoding to unicode
-                Console.InputEncoding = Encoding.Unicode;
-                Console.OutputEncoding = Encoding.Unicode;
-
-
-                // Configure translation
-
-
-                // Configure speech
-                
-
-                string targetLanguage = "";
-                while (targetLanguage != "quit")
-                {
-                    Console.WriteLine("\nEnter a target language\n fr = French\n es = Spanish\n hi = Hindi\n Enter anything else to stop\n");
-                    targetLanguage=Console.ReadLine().ToLower();
-                    if (translationConfig.TargetLanguages.Contains(targetLanguage))
-                    {
-                        await Translate(targetLanguage);
-                    }
-                    else
-                    {
-                        targetLanguage = "quit";
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+          Console.WriteLine("\nEnter a target language\n fr = French\n es = Spanish\n hi = Hindi\n Enter anything else to stop\n");
+          targetLanguage = Console.ReadLine().ToLower();
+          if (translationConfig.TargetLanguages.Contains(targetLanguage))
+          {
+            await Translate(targetLanguage);
+          }
+          else
+          {
+            targetLanguage = "quit";
+          }
         }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+    }
 
-        static async Task Translate(string targetLanguage)
-        {
-            string translation = "";
+    static async Task Translate(string targetLanguage)
+    {
+      string translation = "";
 
-            // Translate speech
+      // Translate speech
+      using AudioConfig audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+      using TranslationRecognizer translator = new TranslationRecognizer(translationConfig, audioConfig);
+      Console.WriteLine("Speak now...");
+      TranslationRecognitionResult result = await translator.RecognizeOnceAsync();
+      Console.WriteLine($"Translating '{result.Text}'");
+      translation = result.Translations[targetLanguage];
+      Console.OutputEncoding = Encoding.UTF8;
+      Console.WriteLine(translation);
 
-
-            // Synthesize translation
-
-
-        }
+      // Synthesize translation
+      var voices = new Dictionary<string, string>
+      {
+        ["fr"] = "fr-FR-HenriNeural",
+        ["es"] = "es-ES-ElviraNeural",
+        ["hi"] = "hi-IN-MadhurNeural"
+      };
+      speechConfig.SpeechSynthesisVoiceName = voices[targetLanguage];
+      using SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer(speechConfig);
+      SpeechSynthesisResult speak = await speechSynthesizer.SpeakTextAsync(translation);
+      if (speak.Reason != ResultReason.SynthesizingAudioCompleted)
+      {
+        Console.WriteLine(speak.Reason);
+      }
 
     }
+
+  }
 }
